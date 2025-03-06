b509225ac40b332371186beba2d8140efae04392
Vyach <vyacheslav.ssok000@gmail.com>
2025-03-06 23:55:13 +0700

    feat(main): display user cards

--- app/routes/main.py
+++ app/routes/main.py
@@ -1,7 +1,10 @@
 from flask import Blueprint, render_template
 
+from app.models import User
+
 main = Blueprint('main', __name__)
 
 @main.route('/')
 def index():
-    return render_template('main/index.html', title='Home')
+    users = User.query.all()
+    return render_template('main/index.html', title='Home', users=users)
